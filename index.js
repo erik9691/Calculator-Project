bcec80f6e4cfc2f31c6034aca47abdd4b98d9d82
erik9691 <gagwaffle@gmail.com>
2025-02-20 20:44:36 -0300

    Added operate function
    It uses the previous functions to run a chosen operation

--- index.js
+++ index.js
@@ -21,3 +21,25 @@ function divide(num1,num2)
     const res = num1 / num2;
     return res;
 }
+
+let number1;
+let operator;
+let number2;
+
+function operate(num1, operator, num2)
+{
+    switch (operator) {
+        case "+":
+            add(num1,num2)
+            break;
+        case "-":
+            subtract(num1,num2)
+            break;
+        case "*":
+            multiply(num1,num2)
+            break;
+        case "/":
+            divide(num1,num2)
+            break;
+    }
+}

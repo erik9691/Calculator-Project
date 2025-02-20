1f32bfd7c76e5b2dd1cd8de012f00df1a5673eec
erik9691 <gagwaffle@gmail.com>
2025-02-20 20:38:17 -0300

    Basic +-*/ functions

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(num1, num2)
+{
+    const res = num1 + num2;
+    return res;
+}
+
+function subract(num1,num2)
+{
+    const res = num1 - num2;
+    return res;
+}
+
+function multiply(num1,num2)
+{
+    const res = num1 * num2;
+    return res;
+}
+
+function divide(num1,num2)
+{
+    const res = num1 / num2;
+    return res;
+}

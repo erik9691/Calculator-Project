7cb22bf80381496558eb3246617cb48f762d6239
erik9691 <gagwaffle@gmail.com>
2025-02-21 03:17:52 -0300

    Can succesfully calculate once
    Still quite broken

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-let number1;
+let number1 = "";
 let operator;
-let number2;
+let number2 = "";
+let result;
+let atNumber2 = false;
 
 let screenContent;
 
@@ -14,18 +16,54 @@ buttons.forEach((button) =>
 
 function placeDigit(e)
 {
-    console.log("Pressed!");
-    screen.innerText += e.target.className;
+    if (e.target.className === "=")
+    {
+        calculate();
+    }
+    else
+    {
+        screen.innerText += e.target.className;
+        screenContent = screen.innerText;
+    }
+}
+
+function calculate()
+{
+    for (let i = 0; i < screenContent.length; i++)
+    {
+        console.log(screenContent.charAt(i))
+        if (screenContent.charAt(i) !== "+" && screenContent.charAt(i) !== "-" && screenContent.charAt(i) !== "x" && screenContent.charAt(i) !== "/")
+        {
+            if (!atNumber2)
+            {
+                console.log("num1");
+                number1 += screenContent.charAt(i).toString();
+            }
+            else
+            {
+                number2 += screenContent.charAt(i).toString();
+            }
+        }
+        else
+        {
+            operator = screenContent.charAt(i);
+            atNumber2 = true;
+        }
+    }
+    atNumber2 = false
+    screen.innerText = operate(parseInt(number1),operator,parseInt(number2))
     screenContent = screen.innerText;
 }
 
 function add(num1, num2)
 {
+    console.log(num1)
+    console.log(num2)
     const res = num1 + num2;
     return res;
 }
 
-function subract(num1,num2)
+function subtract(num1,num2)
 {
     const res = num1 - num2;
     return res;
@@ -45,18 +83,23 @@ function divide(num1,num2)
 
 function operate(num1, operator, num2)
 {
-    switch (operator) {
+    let res;
+
+    switch (operator) 
+    {
         case "+":
-            add(num1,num2)
+            res = add(num1,num2)
             break;
         case "-":
-            subtract(num1,num2)
+            res = subtract(num1,num2)
             break;
         case "*":
-            multiply(num1,num2)
+            res = multiply(num1,num2)
             break;
         case "/":
-            divide(num1,num2)
+            res = divide(num1,num2)
             break;
     }
+
+    return res;
 }
